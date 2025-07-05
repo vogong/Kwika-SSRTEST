698b56236f453e47322bd1379369f7a28a2dd26d
vaughngavin <vaughng@vasoft.ca>
2025-07-04 22:11:27 -0400

    chore: add trailing whitespace to express.static line in minimal-server.js

--- minimal-server.js
+++ minimal-server.js
@@ -6,7 +6,7 @@ const PORT = 4000;
 const DIST_FOLDER = path.join(__dirname, 'dist/angular-ssr-metadata/browser');
 
 // Serve static files
-app.use(express.static(DIST_FOLDER));
+app.use(express.static(DIST_FOLDER)); 
 
 // All routes should serve index.html
 app.get('*', (req, res) => {
